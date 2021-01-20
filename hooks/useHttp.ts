fda0bf4b203fc26bc99a01bc93bec2ddeedc8c4e
jim <irawan.jemmy@gmail.com>
2021-01-20 11:46:41 +0800

    can set the response type for useFetch now

--- hooks/useHttp.ts
+++ hooks/useHttp.ts
@@ -6,16 +6,17 @@ import { useIsMounted } from './utils';
 
 type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
-type Resp = {
+type Resp<T> = {
   status: number;
   ok: boolean;
-  data: any;
+  data?: T;
+  error?: string;
 };
 
-export type FetchState = {
+export type FetchState<T> = {
   loading: boolean;
-  result: any;
-  submit: (body?: any | null) => Promise<Resp>;
+  result: T;
+  submit: (body?: any | null) => Promise<Resp<T>>;
 };
 
 /**
@@ -23,16 +24,16 @@ export type FetchState = {
  * TODO: more docs coming soon
  * 
  * @param method the Http Method (GET,POST,PUT,DELETE)
- * @param endpoint the rest api endpoint without the prefix forward slash
+ * @param endpoint the rest api endpoint without the prefix forward slash (/)
  */
-export const useFetch = (method: Method, endpoint: string): FetchState => {
+export const useFetch = <T>(method: Method, endpoint: string): FetchState<T> => {
   const { apiUrl, checkToken, tenantKey } = useAppProvider();
 
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState(undefined);
   const mounted = useIsMounted();
 
-  const submit = async (body?: any | null): Promise<Resp> => {
+  const submit = async (body?: any | null): Promise<Resp<T>> => {
     setLoading(true);
 
     // if tenantKey is defined, then set it with the cur window subdomain as the value.
@@ -54,13 +55,14 @@ export const useFetch = (method: Method, endpoint: string): FetchState => {
     const status = response.status;
     const ok = response.ok;
     const data = await response.json();
+    const error = data?.error;
     // dont change state when no longer mounted
     if (mounted()) {
       setLoading(false);
       setResult(data);
     }
 
-    return { status, ok, data };
+    return { status, ok, data, error };
   };
 
   return { loading, result, submit };
