a503cca0392bc4c37a0ba82c782268db3eb50594
jim <irawan.jemmy@gmail.com>
2021-03-09 15:02:53 +0800

    feat(form): added FormFile for uploading files

--- scripts/extensions/generics.ts
+++ scripts/extensions/generics.ts
@@ -1,3 +1,7 @@
+import { FileModel } from '~w-common/hooks/useFileUpload';
+
+import { lastSplit } from './strings';
+
 /**
  * Useful when you need to call method before returning the value of t itself afterwards.
  *
@@ -74,3 +78,37 @@ export function getValue<T>(obj: Record<string, T>, key): T | undefined {
 
   return undefined;
 }
+
+export const formatBytes = (bytes: number, decimals = 2) => {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+};
+
+export const saveAs = (file: FileModel): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', file.url, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = () => {
+      const f = new Blob([xhr.response], { type: file.type });
+      const a = document.createElement('a');
+      a.setAttribute('download', lastSplit(file.key, '/') || 'file');
+      a.href = window.URL.createObjectURL(f);
+      a.style.display = 'none';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+
+      resolve();
+    };
+    xhr.onerror = reject;
+    xhr.send();
+  });
+};
