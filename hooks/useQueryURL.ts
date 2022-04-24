eb2dbe1c54266982b80cab0bcf5fd8733c0b1896
Jemika Negara <jemikanegara@gmail.com>
2022-04-25 00:28:01 +0800

    fix(use-query-url): add useQueryURLObject hooks for filtering purpose

--- hooks/useQueryURL.ts
+++ hooks/useQueryURL.ts
@@ -1,13 +1,25 @@
+import * as React from 'react';
 import { useLocation } from 'react-router-dom';
 
 /**
  * A custom hook that builds on useLocation to parse
- * the query string for you.
+ * the query string.
  * source: https://reactrouter.com/web/example/query-parameters
  * @returns query object, usage: query.get("name")
  */
 const useQueryURL = (): URLSearchParams => {
-  return new URLSearchParams(useLocation().search);
+  const { search } = useLocation();
+
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+};
+
+/**
+ * useQueryURL returned as object
+ * @returns plain object, usage: query[key]
+ */
+export const useQueryURLObject = (): Record<string, string> => {
+  const query = useQueryURL();
+  return Object.fromEntries(query);
 };
 
 export default useQueryURL;
